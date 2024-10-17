792e600242e9929ff0881c52f1d41f1819f70986
rogers7levi <rogers.levi714@gmail.com>
2024-10-17 14:28:06 +0100

    changed acl

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "canvas-collective" {
   bucket = "canvas-collective"
-  acl    = "private"
+  acl    = "public-read"
 
   website {
     index_document = "index.html"
