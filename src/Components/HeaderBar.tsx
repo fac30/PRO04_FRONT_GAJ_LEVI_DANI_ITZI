430f894c5a3500242c1e4438c3a6ba81dda99124
rogers7levi <rogers.levi714@gmail.com>
2024-10-24 15:34:26 +0100

    yesssss

--- src/Components/HeaderBar.tsx
+++ src/Components/HeaderBar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import { useState } from "react";
 import { CiShoppingCart } from "react-icons/ci";
 import { FaUserCircle } from "react-icons/fa";
 
