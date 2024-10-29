ab2bf0edd8d53eb142ea274e529721a658a60278
Gaajia <m.gajia1998@gmail.com>
2024-10-29 15:10:34 +0000

    taken out the useState from layout as it's within the headerBar component

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
+
 import NavBar from './NavBar';
 import Footer from './Footer';
-import LoginModel from './LoginModel';
+
 import HeaderBar from './HeaderBar';
 
 // Define props for the Layout component
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-	// const [isLoginModelOpen, setIsLoginModelOpen] = useState(false);
-
-	// const openLoginModel = () => setIsLoginModelOpen(true);
-	// const closeLoginModel = () => setIsLoginModelOpen(false);
+ 
 
 	return (
 		<>
 			<NavBar />
 			<HeaderBar/>
-				{/* <button className="flex " onClick={openLoginModel}>Open Login</button>
-			<LoginModel isOpen={isLoginModelOpen} onClose={closeLoginModel} />
-			 */}
 			<main>{children}</main>
 			<Footer />
 		</>
