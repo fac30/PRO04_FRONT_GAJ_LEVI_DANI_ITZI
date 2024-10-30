62de19406703ebc49e2bceeba4edca6ae13a889b
Itziar Cantero <itziar.cantero26@gmail.com>
2024-10-30 21:02:42 +0000

    Add z-index to the navbar and the carousel, as the dropdown menu was hidden behind the images

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ import Carousel from "../Components/Carousel";
 function HomePage() {
   return (
     <Layout>
-      <section className="carousel w-[60%] m-auto">
+      <section className="carousel w-[60%] m-auto z-10">
         <Carousel />
       </section>
     </Layout>
